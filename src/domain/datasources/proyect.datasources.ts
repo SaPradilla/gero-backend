fccc658571e5dc4d5066bc4320131d9e5c396cb8
SaPradilla <andress_garces@soy.sena.edu.co>
2024-05-18 08:57:53 -0500

    feat: proyect domain

--- src/domain/datasources/proyect.datasources.ts
+++ src/domain/datasources/proyect.datasources.ts
@@ -1,8 +1,9 @@
+import { EditProyectDto } from "../dtos/proyect/edit-proyecto.dto";
 import { RegisterProyectDto } from "../dtos/proyect/register-proyect.dto";
 import { ProyectEntity } from "../entities/proyect.entity";
 
 export abstract class ProyectDatasource{
 
     abstract register( registerProyect: RegisterProyectDto  ) : Promise<ProyectEntity>
-    // abstract edit()
-}
+    abstract edit( editProyect: EditProyectDto) : Promise<ProyectEntity>
+}   
